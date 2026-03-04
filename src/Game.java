f9b792be86fad9cd860270665b68e62531842a64
Farzaneh Soghani <farzanehsoghani@gmail.com>
2026-03-04 21:46:12 +0100

    Rename project from GamePieceTest to Game

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GamePieceTest {
+public class Game {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
